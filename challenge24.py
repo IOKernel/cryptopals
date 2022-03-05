a1daa4f346b1fdb54306520a5087e5b99d5cd49d
IOKernel <kiraliciousness@gmail.com>
2022-03-05 23:31:21 +0400

    challenge24

--- challenge24.py
+++ challenge24.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from utils import Random, xor
-
+from os import urandom
+from random import randint
 # --------------------------------------------------------
 # ---------------------- constants -----------------------
 # --------------------------------------------------------
@@ -26,12 +27,32 @@ def encrypt(plaintext: bytes, seed: int) -> bytes:
     length = len(plaintext)
     keystream = get_keystream(seed, length)
     return xor(plaintext, keystream)
+
+def bruteforce_seed(keystream_known, length, seed_size = 16):
+    for i in range(2**seed_size):
+        seed = i
+        keystream = get_keystream(seed, length)
+        if keystream_known in keystream:
+            print('FOUND')
+            return seed
+
 # --------------------------------------------------------
 # ------------------------- main -------------------------
 # --------------------------------------------------------
 def main():
+    # setting the seed
+    # first part of the encryption/decryption test
     seed = 0xDEAD
-    ciphertext = encrypt(b'hello world!', seed)
-    print(ciphertext)
+    prefix = urandom(randint(1,10))
+    plaintext = b'A'*14
+    ciphertext = encrypt(prefix + plaintext, seed)
+    # recovering the seed
+    keystream_known = xor(ciphertext[-14:], plaintext)
+    recovered_seed = bruteforce_seed(keystream_known, len(plaintext+prefix))
+    print(f"{recovered_seed = }\nIn hex = {hex(recovered_seed)}")
+    # generate password reset token
+    # check if password token is encrypted with MT19937
+    # psuedo-code
+
 if __name__ == "__main__":
     main()
