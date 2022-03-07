b9cb99b238a358b77c2362d1b02fefcc64c6f921
IOKernel <kiraliciousness@gmail.com>
2022-03-07 20:33:34 +0400

    restarting

--- challenge28.py
+++ challenge28.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
-from sha1 import sha1
-from aes import random_bytes_gen
+
 # --------------------------------------------------------
 # ---------------------- functions -----------------------
 # --------------------------------------------------------
-def sha1mac(message: bytes) -> bytes:
-    #key = random_bytes_gen(16)
-    key = b'\xa0f\xa6\xe5\x16h\n\xf3\xd9\x84\x0c\xefz\x05\xea_'
-    return sha1(key+message)
+
 # --------------------------------------------------------
 # ------------------------- main -------------------------
 # --------------------------------------------------------
 
 def main():
-    digest = sha1mac(b'test')
-    print(digest)
+    pass
 
 if __name__ == "__main__":
     main()
