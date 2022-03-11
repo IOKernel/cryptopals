4da86581d8d9c09cd1f667cc788aefd7719262c6
IOKernel <kiraliciousness@gmail.com>
2022-03-11 15:48:31 +0400

    chal 29 solution, works only for some keys, random

--- challenge29.py
+++ challenge29.py
@@ -4,8 +4,11 @@ from utils import ans_check
 # --------------------------------------------------------
 # ---------------------- functions -----------------------
 # --------------------------------------------------------
-def md_padding(keylen_guess: int, og_message: str, new_message: str):
-    padding = bin((keylen_guess + len(og_message) + len(new_message)) * 8)[2:].rjust(64, '0')
+def md_padding(keylen_guess: int, og_message: bytes):
+    full_msg_len = keylen_guess + len(og_message) 
+    ml = bin(full_msg_len * 8)[2:].rjust(64, '0')
+    klen = 448 - ((full_msg_len*8)+1)%512
+    padding = '1' + '0'*klen + ml
     return padding
 
 def break_chunks(data, chunk_size: int) -> list:
@@ -13,25 +16,46 @@ def break_chunks(data, chunk_size: int) -> list:
             data = bin(data)[2:].rjust(512, '0')
         return [int(data[i:i+chunk_size],2) for i in range(0, len(data), chunk_size)]
 
+def bitstring_to_bytes(s):
+    v = int(s, 2)
+    b = bytearray()
+    while v:
+        b.append(v & 0xff)
+        v >>= 8
+    return bytes(b[::-1])
+
 # --------------------------------------------------------
 # ------------------------- main -------------------------
 # --------------------------------------------------------
 
 def main():
-    key = "mango"
-    og_str = "comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon"
-    new_str = ";admin=true"
-    max_keylen_guess = 8
+    # init
+    key = b"mango"
+    og_str = b"comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon"
+    new_str = b";admin=true"
+    max_keylen_guess = 12
+
+    # get original hash with MAC
+    print('Original sha----------------')
     og_sha = bin(int(sha1(key+og_str), 16))[2:]
     og_chunks = break_chunks(og_sha, 32)
-    print(og_chunks)
-    # correct sha if we use original key
-    correct_sha = sha1(key+og_str+new_str)
-    for i in range(1, max_keylen_guess):
+
+    for i in range(5, 6):
         print(f"KEY LENGTH = {i}-----------------------")
-        padding = md_padding(i, og_str, new_str)
-        print(padding)
-        new_sha = sha1(new_str, og_chunks, padding)
-        ans_check(new_sha, correct_sha)
+        # get og_padding + len in bytes format
+        padding = md_padding(i, og_str)
+        padding_bytes = bitstring_to_bytes(padding)
+
+        # correct sha if we use original key
+        # has to be key + og_str + padding + length + new_str
+        forged_padding = md_padding(i, og_str+padding_bytes+new_str)
+        print('forged sha----------------')
+        new_sha = sha1(new_str, og_chunks, padding=forged_padding)
+        print('correct_sha----------------')
+        correct_sha = sha1(key+og_str+padding_bytes+new_str)
+        print(f"{correct_sha= }")
+        print(f"{new_sha=     }")
+        if ans_check(new_sha, correct_sha):
+            break
 if __name__ == "__main__":
     main()
