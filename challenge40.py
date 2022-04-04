f3809d08f5bbfd09cb2bbf70596dd2cdf33d28be
IOKernel <kiraliciousness@gmail.com>
2022-04-04 18:00:12 +0400

    removed eek and invmod test funcs found from stack

--- challenge40.py
+++ challenge40.py
@@ -8,31 +8,6 @@ from gmpy2 import root
 # --------------------------------------------------------
 # ---------------------- functions -----------------------
 # --------------------------------------------------------
-def extended_gcd(a, b):
-    x, y = 0, 1
-    lastx, lasty = 1, 0
-
-    while b:
-        a, (q, b) = b, divmod(a, b)
-        x, lastx = lastx - q * x, x
-        y, lasty = lasty - q * y, y
-
-    return (lastx, lasty, a)
-
-
-def mul_inv(a, b):
-    b0 = b
-    x0, x1 = 0, 1
-    if b == 1:
-        return 1
-    while a > 1:
-        q = a // b
-        a, b = b, a % b
-        x0, x1 = x1 - q * x0, x0
-    if x1 < 0:
-        x1 += b0
-    return x1
-
 def crt(pairs):
     # chinese remainder theorem
     N = 1
