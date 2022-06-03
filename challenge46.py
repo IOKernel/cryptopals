941d173aa51ae321737003bea3896b3d13a9413b
IOKernel <github@cryptik.io>
2022-06-03 19:13:44 +0400

    challenge 46 solution

--- challenge46.py
+++ challenge46.py
@@ -8,7 +8,7 @@ from base64 import b64decode
 # --------------------------------------------------------
 # ---------------------- functions -----------------------
 # --------------------------------------------------------
-def oracle(rsa: Rsa, ct: bytes) -> bool:
+def oracle(rsa: Rsa, ct: int) -> bool:
     """
     input: rsa = Rsa object
            ct = ciphertext
@@ -24,9 +24,29 @@ def break_rsa(rsa: Rsa, ct: int) -> bytes:
     output: plaintext
     """
     # if c1 * c2 = ct, then pt = p1 * p2. In this case p2  = 2 * i
+    # so if we double the ciphertext, c = c * pow(2, e, n). then:
+    # the plaintext is also doubled, pt = pt * 2
     e, n = rsa.getPubKey()
-    lower_bound, upper_bound = 0, n
 
+    lower_bound, upper_bound = 1, n - 1
+    iteration = 0
+    multiplier = pow(2, e, n)
+    print("[*] Trying to find the plaintext...")
+    while lower_bound < upper_bound: # or for _ in range(1024)
+        print("-" * 50)
+        print(f"[*] Iteration {iteration}")
+        iteration += 1
+
+        mid = (lower_bound + upper_bound) // 2
+        ct *= multiplier
+        # I can't get it to return correct last byte b'a'
+        if oracle(rsa, ct):
+            upper_bound = mid - 1
+        else:
+            lower_bound = mid + 1
+        print(int2bytes(upper_bound))
+
+    return int2bytes(upper_bound)
 # --------------------------------------------------------
 # ------------------------- main -------------------------
 # --------------------------------------------------------
@@ -35,6 +55,7 @@ def main():
     pt = b64decode("VGhhdCdzIHdoeSBJIGZvdW5kIHlvdSBkb24ndCBwbGF5IGFyb3VuZCB3aXRoIHRoZSBGdW5reSBDb2xkIE1lZGluYQ==")
     ct, _ = rsa.encrypt(pt)
     break_rsa(rsa, ct)
+    
 
 if __name__ == "__main__":
     main()
